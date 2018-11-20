79f1872603c7f8e052bb82df274bc7aeb0dea6d2
jdouillard <jay.doo@gmail.com>
2018-11-20 15:46:03 -0800

    Initial commit

---
+++ dataimport.py
@@ -0,0 +1,51 @@
+import sqlite3
+import csv
+ 
+conn = sqlite3.connect('static/counts.sqlite')
+ 
+cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS counts") 
+cur.execute("""CREATE TABLE counts(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    countID INTEGER,
+    onStreet TEXT,
+    location TEXT,
+    xStreet TEXT,
+    countDirection TEXT,
+    heading TEXT,
+    countStart TEXT,
+    totalCount INTEGER,
+    femleCount INTEGER,
+    childCount INTEGER)""")
+ 
+with open('static/BikeCountsHourly.csv', "rb") as csv_file:
+    reader = csv.reader(csv_file)
+    #Skip the header 
+    next(reader)
+    for row in reader:
+        to_db = [
+        unicode(row[0], "utf8"), 
+        unicode(row[1], "utf8"),
+        unicode(row[2], "utf8"),
+        unicode(row[3], "utf8"),
+        unicode(row[4], "utf8"),
+        unicode(row[5], "utf8"),
+        unicode(row[6], "utf8"),
+        row[7],
+        row[8],
+        row[9]
+        ]
+        cur.execute("""INSERT INTO counts (
+            countID,
+            onStreet,
+            location,
+            xStreet,
+            countDirection,
+            heading,
+            countStart,
+            totalCount,
+            femleCount,
+            childCount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""", to_db)
+cur.execute("CREATE INDEX countID_idx on counts(countID);")
+conn.commit()
+conn.close()
---
+++ wsgi.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template,jsonify,json,g
+import sqlite3
+app = Flask(__name__)
+
+DATABASE = 'static/counts.sqlite'
+
+def get_db(row=True):
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        if row==True:
+            db.row_factory=sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chart/<int:countID>")
+def chart(countID):
+
+    return render_template("chart.html",countID=countID)    
+
+@app.route("/table/<int:countID>")
+def table(countID):
+    counts=get_db().execute('''SELECT countID,onStreet,location,xStreet,
+                            strftime('%Y-%m-%d',countStart) as countDate,
+                            strftime('%H:%M',countStart) as countTime, heading, totalCount 
+                            FROM counts WHERE countID=? ORDER BY countStart''',(countID,)).fetchall()
+    return render_template("table.html",counts=counts)         
+
+@app.route("/data/v1.0/<int:countID>", methods=['GET'])
+def data(countID):
+    counts=get_db().execute('SELECT * FROM counts WHERE countID=?',(countID,)).fetchall()
+    return jsonify([dict(ix) for ix in counts])
+
+if __name__ == "__main__":
+    application.run()
